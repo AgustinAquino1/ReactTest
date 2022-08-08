fdd64b19e369f6c3ae8f1a2a51b3def454b5b6a4
AgustinAquino1 <agustin.aquino1@gmail.com>
2022-08-08 13:44:45 -0300

    custom hook

--- src/components/ComponentsStates/Status.js
+++ src/components/ComponentsStates/Status.js
@@ -1,38 +1,12 @@
 import React, { useState, useEffect} from "react";
 import axios, { AxiosError } from "axios";
 import './Components.css';
+import useFetchData from "../../hooks/useFetchData";
 
 const Status = ({apiName}) => {
 
-const [state, setState] = useState([]);
-const [error, setError] =useState ([])
-const [clockState, setClockState] = useState();
+const {state, clockState, error} = useFetchData(apiName)
 
-const modifyRefreshInterval = 15 /* If you want to change the refresh interval, must modify this number. Is written in seconds*/
-  
-        
-  const fetchData = async () => {
-    const { data } = await axios.get(
-      `https://api.factoryfour.com/${apiName}/health/status`
-      )
-        .catch(function (error) {
-          setError(error) 
-        });
-        setState(data);
-        const date = new Date(data.time);
-  
-        setClockState(date.toLocaleTimeString());
-        
-        
-      };
-      
-      useEffect(() => {
-
-      fetchData()
-      setInterval(() => {
-        fetchData()     
-      }, (1000 * modifyRefreshInterval));
-    }, []);
     
    
   
---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect} from "react";
+import axios, { AxiosError } from "axios";
+
+const useFetchData = (apiName) => {
+const [state, setState] = useState([]);
+const [error, setError] =useState ([])
+const [clockState, setClockState] = useState();
+
+const modifyRefreshInterval = 15 /* If you want to change the refresh interval, must modify this number. Is written in seconds*/
+
+    const fetchData = async () => {
+        const { data } = await axios.get(
+          `https://api.factoryfour.com/${apiName}/health/status`
+          )
+            .catch(function (error) {
+              setError(error) 
+            });
+            setState(data);
+            const date = new Date(data.time);
+            console.log("🚀 ~ date", date)
+      
+            setClockState(date.toLocaleTimeString());
+            
+            
+          };
+          
+          useEffect(() => {
+    
+          fetchData()
+          setInterval(() => {
+            fetchData()     
+          }, (1000 * modifyRefreshInterval));
+        }, []);
+
+        return {state, error, clockState}
+}
+export default useFetchData
